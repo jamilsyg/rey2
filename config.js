1798d4571d7f8db0d5adb27879bba2c4dabce8f2
Andik Fr <91518502+SkylarKaf@users.noreply.github.com>
2022-05-26 23:48:48 +0700

    Deleted

--- config.js
+++ config.js
@@ -52,11 +52,6 @@ global.limitawal = {
 global.gifmp4 = "https://a.uguu.se/HBJheKuH.mp4",
 global.menuimg = "https://uploader.caliph.my.id/file/mNV5rMkeoE.jpg"
 
-global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
